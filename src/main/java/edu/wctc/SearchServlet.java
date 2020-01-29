6ef741955cb4af885f109065e5e4acc8be46ecb1
Jake Mierow <jake.mierow@icloud.com>
2020-01-28 22:40:04 -0600

    search servlet

---
+++ src/main/java/edu/wctc/SearchServlet.java
@@ -0,0 +1,88 @@
+package edu.wctc;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.*;
+
+@WebServlet(name = "SearchServlet")
+public class SearchServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rset = null;
+
+        String speciesName = request.getParameter("species");
+
+
+        try {
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+
+
+            String absPath = getServletContext().getRealPath("/") + "../../db";
+
+            conn = DriverManager.getConnection("jdbc:derby:" + absPath, "jake", "jake");
+
+            String sql = "select name, age, speciesname from pet where speciesname = ?";
+
+            stmt = conn.prepareStatement(sql);
+
+            stmt.setString(1, speciesName);
+
+
+            rset = stmt.executeQuery("select name, age, speciesname from pet");
+
+
+            StringBuilder sb = new StringBuilder("<html><body>");
+            sb.append("<ui>");
+            while (rset.next()) {
+                sb.append("<li");
+                String name = rset.getString("speciesname");
+                int age = rset.getInt("age");
+                String species = rset.getString(3);
+                sb.append(name + "," + age + "," + "," + species);
+                sb.append("<li>");
+            }
+            sb.append("</ul");
+            sb.append("</body></html>");
+
+            response.setContentType("text/html");
+
+            response.getWriter().print(sb.toString());
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (rset != null) {
+                try {
+                    rset.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
